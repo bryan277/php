068325b43b39f62cf74a224561836462f160f197
hfvos90 <hfvos90@gmail.com>
2019-07-08 10:30:27 -0700

    first commit

---
+++ introTo.php
@@ -0,0 +1,29 @@
+<?php
+echo "Hello, World!";
+// type php terminal.php
+echo "\nRasmus is the creater of PHP";
+echo "\nLearn to have \"fun\"";
+echo "\nclearCode" . "cademy";
+// Hello, World!
+// Rasmus is the creater of PHP                               pwd
+// Learn to have "fun"
+// Codecademy
+echo "\nMy name is:" . " Hans";
+echo "\n" . "tur" . "duck" . "en";
+// My name is: Hans
+// turducken
+
+//var
+$mood = ":)";
+$biography = "\nI like to code";
+$favorite_food = "\n" . "tur" . "duck" . "en";
+
+echo $favorite_food;
+
+$name = "Hans";
+$language = "PHP";
+
+echo "My name is " . $name;
+//My name is Hans
+echo "\n I'm currently learning " . $language;
+//I'm currently learning PHP
---
+++ variableParsing.php
@@ -0,0 +1,19 @@
+<?php
+
+$noun = "man";
+  $adjective = "happy";
+  $verb = "pizza";
+
+  echo "The world's most beloved $noun 
+  was very $adjective and loved to $verb 
+  every single day.";
+
+  echo "\nI have always been obsessed 
+  with ${noun}s. I'm ${adjective}ish. 
+  I'm always ${verb}ing.";
+
+//   The world's most beloved man
+//   was very happy and loved to pizza
+//   every single day.
+//   I have always been obsessed with mans. I'm happyish.
+//   I'm always pizzaing.
